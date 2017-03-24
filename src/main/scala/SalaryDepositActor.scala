0c1deba029d7f441a42396c17629c3cd99b233f7
mahesh chand <shivangi.gupta@knoldus.in>
2017-03-24 16:44:06 +0530

    second commit

--- src/main/scala/SalaryDepositActor.scala
+++ src/main/scala/SalaryDepositActor.scala
@@ -5,21 +5,28 @@ import UserAccountService._
 class SalaryDepositActor extends Actor {
 
   def receive = {
-  case (account_no: Int, uname: String, salary: Double) => {
-    listOfUser.map(x => if(x.account_number == account_no)
-       {
-          val updated = x.copy(initial_amount = x.initial_amount + salary)
-          listOfUser +=updated
-       }
+  case (account_no: Int,uname: String, salary: Double) => {  if( updateSalary(account_no,salary,listOfUser.toList))
+                                                              println("successfully deposited")
+                                                               else println("transaction failed")
+                                                                }
 
-    )
-    println("successfully deposited")
-
-    }
-        case _ => println("unknown request")
-}
 
   }
+  def updateSalary(account_no:Int,salary:Double,list:List[CustomerData]):Boolean = {
+    var flag = false
+    list match {
+      case head::tail if(head.account_number == account_no) => {
+         var update = head.copy(initial_amount = head.initial_amount + salary)
+        listOfUser.append(update)
+        flag = true
+      }
+      case _::tail => updateSalary(account_no,salary,tail)
+    }
+    flag
+  }
+
+
+}
 
 object SalaryDepositActor {
 
--- src/main/scala/UserAccountService.scala
+++ src/main/scala/UserAccountService.scala
@@ -1,41 +1,15 @@
 import scala.collection.mutable.ListBuffer
 
 
+
+
 class UserAccountService {
 
-
-
-//  def createAccount(user:Customer) =
-//  {
-//    val usr = CustomerData(account_no,user.account_holder_name,user.address,user.uname,user.initial_amount)
-//    account_no += 1
-//    val flag = listOfUser.map(x => if(x.uname == user.uname) true else false)
-//
-//    if(flag.forall(_ == false)) {
-//      listOfUser += usr
-//      println("user is successfully added")
-//      println("User Details:")
-//      print(usr.account_number + "\n" + user.account_holder_name + "\n" + user.uname + "\n" + user.address + "\n" + user.initial_amount)
-//    }
-//    else
-//      println("\nusername already exist")
-//
-//  }
-
-
-}
-object UserAccountService{
-  val listOfUser = new ListBuffer[CustomerData]()
-  var account_no = 1
 }
 
-//object UserAccountService extends App{
-//
-//  val usr = Customer("mahesh","Delhi","mahesh24",100)
-//  val obj = new UserAccountService
-//  obj.createAccount(usr)
-//  val usr1 = Customer("mahesh","Delhi","mahesh4",100)
-//  obj.createAccount(usr1)
-//
-//
-//}
+object UserAccountService {
+  //val listOfUser:ListBuffer[Map[String ,CustomerData]] = new ListBuffer[Map[String , CustomerData]] ()
+  val listOfUser = new ListBuffer[CustomerData]
+   var account_no = 1
+
+}
